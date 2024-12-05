85ad70f81e4ed07983d17b218492877acec23261
eala-lah <eala-lah@student.hive.fi>
2024-12-05 15:14:29 +0200

    revert

--- inc/libft.h
+++ inc/libft.h
@@ -6,21 +6,13 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 14:09:50 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/12/05 15:01:30 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/12/05 15:14:23 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-/* Includes necessary libraries for the project.
- * - Limits.h: Defines constants related to system limits (e.g., INT_MAX).
- * - Errno.h: Provides error codes and descriptions for system calls.
- * - Stdlib.h: Includes functions for memory allocation and string manipulation.
- * - Unistd.h: Contains system calls like read and write.
- * - Stdint.h: Defines fixed-width integer types.
- * - Stdbool.h: Provides a boolean type for C.
- */
 # include <limits.h>
 # include <errno.h>
 # include <stdlib.h>
@@ -28,9 +20,8 @@
 # include <stdint.h>
 # include <stdbool.h>
 
-/* --- Check Functions ---
- * Functions for validating different types of characters and data.
- */
+// CHECKS
+
 int			ft_abs(int n);
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
@@ -41,34 +32,29 @@ int			ft_isprint(int c);
 int			ft_issign(char c);
 int			ft_isspace(int c);
 
-/* --- Converter Functions ---
- * Functions for converting data types and manipulating strings.
- */
+// CONVERTERS
+
 double		ft_atof(const char *str);
 int			ft_atoi(const char *str);
 char		*ft_itoa(int n);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
 
-/* --- Error Handling ---
- * Function for printing error messages.
- */
+// ERRORS
+
 void		ft_error(char *msg);
 
-/* --- Get Next Line Function ---
- * Function for reading a line from a file descriptor.
- */
+// GET NEXT LINE
+
 char		*get_next_line(int fd);
 
-/* --- Linked List Functions ---
- * Functions for manipulating linked lists.
- */
+// LISTS
+
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 void		ft_lstclear(t_list **lst, void (*del)(void *));
@@ -79,9 +65,8 @@ t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list		*ft_lstnew(void *content);
 int			ft_lstsize(t_list *lst);
 
-/* --- Memory Functions ---
- * Functions for manipulating memory.
- */
+// MEMORIES
+
 void		ft_bzero(void *s, size_t n);
 void		*ft_calloc(size_t count, size_t size);
 char		*ft_free(char **str);
@@ -91,22 +76,19 @@ void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memset(void *b, int c, size_t len);
 
-/* --- Printf Function ---
- * Function for formatted output to stdout.
- */
+// PRINTF
+
 int			ft_printf(const char *str, ...);
 
-/* --- Output Functions ---
- * Functions for writing data to file descriptors.
- */
+// PUTS
+
 void		ft_putchar_fd(char c, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 void		ft_putstr_fd(char *s, int fd);
 
-/* --- String Functions ---
- * Functions for manipulating strings.
- */
+// STRINGS
+
 size_t		ft_nbrlen(int nbr);
 char		*ft_newstr(size_t n);
 char		**ft_split(char const *s, char c);
