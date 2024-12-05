a7610efd42cb4fc7f453001813fcd0d5cd59be67
eala-lah <eala-lah@student.hive.fi>
2024-12-05 15:06:50 +0200

    added comments

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,17 +6,23 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:22:51 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/07/03 15:04:26 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/12/05 14:58:57 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+/* Includes necessary libraries for the project.
+ * - stdarg.h: For handling variable arguments in functions like ft_printf.
+ * - unistd.h: Provides write functionality for output.
+ */
 # include <stdarg.h>
-# include <limits.h>
 # include <unistd.h>
 
+/* --- Function Prototypes ---
+ * Functions responsible for formatted output to stdout.
+ */
 size_t			ft_nbrlen(int nbr);
 int				ft_printchar(char c);
 int				ft_printf(const char *str, ...);
