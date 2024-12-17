5f4b8a6e7ebddd266fbf925363ec9eb219ba3edd
eala-lah <eala-lah@student.hive.fi>
2024-12-17 14:45:22 +0200

    redid comments, the problem was a missing limits.h include in printf that made libft uncompilable.

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,18 +6,26 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:22:51 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/12/17 14:35:07 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/12/17 14:44:56 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+/* Includes necessary libraries for the project.
+ * - stdarg.h: For handling variable arguments in functions like ft_printf.
+ * - unistd.h: Provides write functionality for output.
+ * - limits.h: Defines implementation-specific limits for integer types.
+ */
 # include <stdarg.h>
 # include <unistd.h>
-#include <limits.h>
+# include <limits.h>
 
-size_t			ft_nbrlen(int nbr);
+/* --- Function Prototypes ---
+ * Functions responsible for formatted output to stdout.
+ */
+size_t			nbrlen(int nbr);
 int				ft_printchar(char c);
 int				ft_printf(const char *str, ...);
 int				ft_printhexa(unsigned long n, char format);
