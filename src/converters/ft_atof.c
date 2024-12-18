df726d8df277b51c4b64d2a2e8a128762a18a1c8
eala-lah <eala-lah@student.hive.fi>
2024-12-18 16:01:51 +0200

    added comments

--- src/converters/ft_atof.c
+++ src/converters/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 17:49:34 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/12/18 14:47:19 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/12/18 16:01:24 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,61 +15,46 @@
 /*
  * ft_atof - Converts a string to a floating point number.
  *
- * This function parses a string to produce a floating point number by:
+ * This function converts a string into a floating point number by:
  * 1. Skipping any leading whitespace.
  * 2. Handling an optional sign (positive or negative).
- * 3. Converting the string to a float by processing digits before and after 
- *    a decimal point.
- * The resulting value is returned as a floating point number, where the sign 
- * and decimal precision are properly handled.
+ * 3. Parsing digits before and after a decimal point to calculate the final 
+ * value.
+ * The function accounts for the optional decimal portion and adjusts the result
+ * accordingly by multiplying the decimal part by progressively smaller factors.
  *
  * Parameters:
  * - str: The string to be converted into a floating point number.
  *
  * Returns:
- * - The floating point number represented by the string, accounting for any 
- *   sign and decimal precision.
+ * - The floating point number represented by the string, with correct handling
+ *   of the sign and decimal precision.
  */
-static double	handle_sign_and_whitespace(const char **str)
-{
-	double	sign;
-
-	sign = 1.0;
-	while (**str && ft_isspace(**str))
-		(*str)++;
-	if (ft_issign(**str))
-	{
-		if (**str == '-')
-			sign = -1.0;
-		(*str)++;
-	}
-	return (sign);
-}
-
 double	ft_atof(const char *str)
 {
-	double	result;
-	double	decimal_factor;
+	double	res;
+	double	dec;
 	double	sign;
 
-	result = 0.0;
-	decimal_factor = 0.1;
-	sign = handle_sign_and_whitespace(&str);
-	while (ft_isdigit(*str))
-	{
-		result = result * 10.0 + (*str - '0');
+	res = 0.0;
+	dec = 0.1;
+	sign = 1.0;
+	while (ft_isspace(*str))
 		str++;
-	}
+	if (ft_issign(*str) && *str == '-')
+		sign = -1.0;
+	if (ft_issign(*str))
+		str++;
+	while (ft_isdigit(*str))
+		res = res * 10.0 + (*str++ - '0');
 	if (*str == '.')
 	{
 		str++;
 		while (ft_isdigit(*str))
 		{
-			result += (*str - '0') * decimal_factor;
-			decimal_factor *= 0.1;
-			str++;
+			res += (*str++ - '0') * dec;
+			dec *= 0.1;
 		}
 	}
-	result *= sign;
-	return (result);
+	return (res * sign);
 }
--- src/memories/ft_memset.c
+++ src/memories/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 13:38:39 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/12/18 14:45:30 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/12/18 15:57:16 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
  * ft_memset - Sets a block of memory to a specific byte.
  *
  * This function sets the first `len` bytes of the memory block pointed to by 
- * `b` 
- * to the byte value `c`. It is often used to initialize or clear buffers.
+ * `b` to the byte value `c`. It is commonly used to initialize or clear 
+ * memory, such as setting buffers to zero or another default value.
+ * The function iterates over the memory block and assigns the specified byte 
+ * value `c` to each byte in the block.
  *
  * Parameters:
  * - b: A pointer to the memory block to be set.
